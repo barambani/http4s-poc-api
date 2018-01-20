7b910ce4402427e7031d4e8c5c0fa035a469bc51
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-01-20 17:15:06 +0000

    Refined dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,12 @@
+import sbt.librarymanagement.syntax
+import sbt.librarymanagement.syntax.ExclusionRule
 import sbt.{ModuleID, _}
 
 object Dependencies {
 
+  /*
+   * Versions
+   */
   private val catsVersion       = "1.0.1"
   private val catsEffectVersion = "0.8"
   private val monixVersion      = "3.0.0-M3"
@@ -12,19 +17,42 @@ object Dependencies {
 
   private val http4sExtendVersion = "0.0.5"
 
+  /*
+   * Transitive dependencies to exclude
+   */
+  private val http4sTransitiveDependencies: Seq[ExclusionRule] = Seq(
+    ("org.typelevel", "cats-core_2.12"),
+    ("org.typelevel", "cats-effect_2.12"),
+    ("io.circe",      "circe-generic_2.12"),
+    ("io.circe",      "circe-literal_2.12")
+  ) map (ex => ExclusionRule(ex._1, ex._2))
+
+  private val monixTransitiveDependencies: Seq[ExclusionRule] = Seq(
+    ("org.typelevel", "cats-effect_2.12")
+  ) map (ex => ExclusionRule(ex._1, ex._2))
+
+  private val http4sExtendTransitiveDependencies: Seq[syntax.ExclusionRule] =
+    http4sTransitiveDependencies ++ monixTransitiveDependencies
+
+  /*
+   * Dependencies and compiler plugins
+   */
   val externalDependencies = Seq(
-    "org.typelevel"         %% "cats-core"            % catsVersion withSources(),
-    "org.typelevel"         %% "cats-effect"          % catsEffectVersion withSources(),
-    "io.monix"              %% "monix"                % monixVersion withSources(),
-    "org.scalaz"            %% "scalaz-concurrent"    % scalazVersion withSources(),
-    "org.http4s"            %% "http4s-dsl"           % http4sVersion withSources(),
-    "org.http4s"            %% "http4s-blaze-server"  % http4sVersion withSources(),
-    "org.http4s"            %% "http4s-blaze-client"  % http4sVersion withSources(),
-    "org.http4s"            %% "http4s-circe"         % http4sVersion withSources(),
-    "io.circe"              %% "circe-generic"        % circeVersion withSources(),
-    "io.circe"              %% "circe-literal"        % circeVersion withSources(),
-    "com.github.barambani"  %% "http4s-extend"        % http4sExtendVersion withSources(),
-    "org.scalacheck"        %% "scalacheck"           % scalaCheckVersion % "test" withSources()
+    "org.typelevel"         %% "cats-core"            % catsVersion         withSources(),
+    "org.typelevel"         %% "cats-effect"          % catsEffectVersion   withSources(),
+    "io.monix"              %% "monix"                % monixVersion        excludeAll(monixTransitiveDependencies:_*) withSources(),
+    "org.scalaz"            %% "scalaz-concurrent"    % scalazVersion       withSources(),
+    "org.http4s"            %% "http4s-dsl"           % http4sVersion       excludeAll(http4sTransitiveDependencies:_*) withSources(),
+    "org.http4s"            %% "http4s-blaze-server"  % http4sVersion       withSources(),
+    "org.http4s"            %% "http4s-blaze-client"  % http4sVersion       withSources(),
+    "org.http4s"            %% "http4s-circe"         % http4sVersion       withSources(),
+    "io.circe"              %% "circe-generic"        % circeVersion        withSources(),
+    "io.circe"              %% "circe-literal"        % circeVersion        withSources(),
+    "com.github.barambani"  %% "http4s-extend"        % http4sExtendVersion excludeAll(http4sExtendTransitiveDependencies:_*) withSources()
+  )
+
+  val testDependencies = Seq(
+    "org.scalacheck" %% "scalacheck" % scalaCheckVersion % "test" withSources()
   )
 
   val compilerPlugins: Seq[ModuleID] = Seq(
