e5a112bcd207b1ce5e111a403aefee9dc69e6e61
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-01-28 18:16:42 +0000

    Added circe string decoder and encoder syntax

--- src/main/scala/codecs/ServiceCodec.scala
+++ src/main/scala/codecs/ServiceCodec.scala
@@ -2,18 +2,18 @@ package codecs
 
 import java.time.Instant
 
-import cats.syntax.either._
+import http4s.extend.util.CirceModule._
 import io.circe.{Decoder, Encoder}
 import shapeless.tag
 import shapeless.tag.@@
 
 object ServiceCodec {
 
+  /**
+    * Encoders
+    */
   implicit val instantEncoder: Encoder[Instant] =
     stringEncoder(_.toString)
-
-  implicit val instantDecoder: Decoder[Instant] =
-    stringDecoder(Instant.parse)
 
   implicit def taggedStringEncoder[T]: Encoder[String @@ T] =
     stringEncoder(identity)
@@ -21,18 +21,12 @@ object ServiceCodec {
   implicit def taggedBigDecimalEncoder[T]: Encoder[BigDecimal @@ T] =
     stringEncoder(_.toString)
 
+  /**
+    * Decoders
+    */
+  implicit val instantDecoder: Decoder[Instant] =
+    stringDecoder(Instant.parse)
+
   implicit def taggedLongDecoder[T]: Decoder[Long @@ T] =
     stringDecoderMap(_.toLong)(tag[T].apply)
-
-
-  private def stringEncoder[A](f: A => String): Encoder[A] =
-    Encoder.encodeString.contramap[A](f)
-
-  private def stringDecoderMap[A, B](f: String => A)(g: A => B): Decoder[B] =
-    Decoder.decodeString emap {
-      str => Either.catchNonFatal(f(str)) leftMap (_ => s"Cannot parse $str to Long") map g
-    }
-
-  private def stringDecoder[A, B](f: String => A): Decoder[A] =
-    stringDecoderMap(f)(identity)
 }
