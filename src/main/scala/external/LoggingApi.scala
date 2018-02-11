d5edad07348456b8c01d933193b374ddede09832
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-02-11 16:47:45 +0000

    Improved logging patterns

--- src/main/scala/external/LoggingApi.scala
+++ src/main/scala/external/LoggingApi.scala
@@ -12,4 +12,5 @@ object LoggingApiImpl extends LoggingApi {
   def error: String => IO[Unit] = ???
   def warning: String => IO[Unit] = ???
   def info: String => IO[Unit] = ???
+  def debug: String => IO[Unit] = ???
 }
