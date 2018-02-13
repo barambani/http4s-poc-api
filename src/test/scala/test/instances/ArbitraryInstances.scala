d3c6e0829a0d54c2facd09d9093f832e60ba88fd
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-02-13 14:08:02 +0000

    Updated htt4s extend with the new NewType for errors

--- src/test/scala/test/instances/ArbitraryInstances.scala
+++ src/test/scala/test/instances/ArbitraryInstances.scala
@@ -1,18 +1,18 @@
 package test.instances
 
 import errors._
-import http4s.extend.Algebra.ExceptionMessage
+import http4s.extend.ExceptionDisplay
 import org.scalacheck.{Arbitrary, Gen}
 
 trait ArbitraryInstances {
 
-  implicit def excMessageArb(implicit ev: Arbitrary[String]): Arbitrary[ExceptionMessage] =
-    Arbitrary { ev.arbitrary flatMap (new ExceptionMessage(_)) }
+  implicit def excMessageArb(implicit ev: Arbitrary[String]): Arbitrary[ExceptionDisplay] =
+    Arbitrary { ev.arbitrary flatMap (ExceptionDisplay(_)) }
 
   implicit def apiErrorArb(implicit ev: Arbitrary[String]): Arbitrary[ApiError] =
     Arbitrary { ev.arbitrary flatMap arbitraryErrorInstance }
 
-  private def arbitraryErrorInstance(implicit ev: Arbitrary[ExceptionMessage]): String => Gen[ApiError] =
+  private def arbitraryErrorInstance(implicit ev: Arbitrary[ExceptionDisplay]): String => Gen[ApiError] =
     m => ev.arbitrary.flatMap {
       em => Gen.oneOf(InvalidParameters(m), InvalidShippingCountry(m), DependencyFailure("test", m), UnknownFailure(em))
     }
