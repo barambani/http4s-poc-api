b14a2b435da0f6983928f6ba4f850458d1049c89
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-02-28 19:53:27 +0000

    Updated versions, improved the parallel map to use the syntax

--- src/main/scala/service/PriceService.scala
+++ src/main/scala/service/PriceService.scala
@@ -4,13 +4,14 @@ import cats.MonadError
 import cats.syntax.apply._
 import cats.syntax.flatMap._
 import http4s.extend.ParEffectful
+import http4s.extend.syntax.parEffectful._
 import interpreters.{Dependencies, Logger}
 import model.DomainModel._
 
 final case class PriceService[F[_] : MonadError[?[_], Throwable] : ParEffectful](dep: Dependencies[F], logger: Logger[F]) {
 
   def prices(userId: UserId, productIds: Seq[ProductId]): F[List[Price]] =
-    (userFor(userId), productsFor(productIds), preferencesFor(userId)).parMap3(priceCalculator.finalPrices).flatten
+    (userFor(userId), productsFor(productIds), preferencesFor(userId)).parMap(priceCalculator.finalPrices).flatten
   
 
   private def userFor(userId: UserId): F[User] =
@@ -31,22 +32,4 @@ final case class PriceService[F[_] : MonadError[?[_], Throwable] : ParEffectful]
 
   private lazy val priceCalculator: PriceCalculator[F] =
     PriceCalculator[F](dep, logger)
-
-  implicit final class Syntax2[FF[_], A, B](t2: (FF[A], FF[B]))(implicit ev: ParEffectful[FF]) {
-
-    def parMap2[R](f: (A, B) => R): FF[R] =
-      ev.parMap2(t2._1, t2._2)(f)
-
-    def parTupled2: FF[(A, B)] =
-      ev.parTupled2(t2._1, t2._2)
-  }
-
-  implicit final class Syntax3[FF[_], A, B, C](t3: (FF[A], FF[B], FF[C]))(implicit ev: ParEffectful[FF]) {
-
-    def parMap3[R](f: (A, B, C) => R): FF[R] =
-      ev.parMap3(t3._1, t3._2, t3._3)(f)
-
-    def parTupled3: FF[(A, B, C)] =
-      ev.parTupled3(t3._1, t3._2, t3._3)
-  }
 }
