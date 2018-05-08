2787e309fbee75c68e45b3e7ceb6819a3d7e8b28
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-05-08 11:53:39 +0100

    Updated versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,10 +10,10 @@ object Dependencies {
     val cats          = "1.1.0"
     val catsEffect    = "0.10"
     val monix         = "3.0.0-RC1"
-    val scalaz        = "7.2.21"
+    val scalaz        = "7.2.22"
     val http4s        = "0.18.7"
     val circe         = "0.9.2"
-    val http4sExtend  = "0.0.31"
+    val http4sExtend  = "0.0.32"
 
     val scalaCheck    = "1.13.5"
     val scalaTest     = "3.0.5"
