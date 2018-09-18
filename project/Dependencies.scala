09e60d239aa339f9973e3ecd3b95b1b799c8d040
Scala steward <scala-steward@timepit.eu>
2018-09-19 01:34:07 +0200

    Update cats-effect to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
    */
   private object versionOf {
     val cats          = "1.2.0"
-    val catsEffect    = "1.0.0-RC"
+    val catsEffect    = "1.0.0"
     val monix         = "3.0.0-RC1"
     val scalaz        = "7.3.0-M23"
     val http4s        = "0.18.16"
