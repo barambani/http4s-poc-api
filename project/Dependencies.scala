74639df3037c092bb3a7238d4557eef9bf33e33b
Scala steward <43047562+scala-steward@users.noreply.github.com>
2018-09-19 03:12:46 +0200

    Update monix to 3.0.0-fbcb270 (#26)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   private object versionOf {
     val cats          = "1.4.0"
     val catsEffect    = "1.0.0-RC"
-    val monix         = "3.0.0-RC1"
+    val monix         = "3.0.0-fbcb270"
     val scalaz        = "7.3.0-M23"
     val http4s        = "0.18.18"
     val circe         = "0.10.0-M2"
