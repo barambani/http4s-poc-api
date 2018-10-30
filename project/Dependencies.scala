e70f5c5f6efabe1f30ccdf87e64224b3ec2f7e9f
Scala steward <43047562+scala-steward@users.noreply.github.com>
2018-10-30 22:29:07 +0100

    Update circe-generic, circe-literal to 0.10.1 (#38)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val monix         = "3.0.0-fbcb270"
     val scalaz        = "7.3.0-M23"
     val http4s        = "0.18.20"
-    val circe         = "0.10.0"
+    val circe         = "0.10.1"
     val http4sExtend  = "0.0.43"
 
     val scalaCheck    = "1.14.0"
