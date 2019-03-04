8e1e749b038a2f5135b996cde9a36e104e8e2a24
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-03-04 20:47:44 +0100

    Update scalatest to 3.0.6 (#48)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val http4sExtend  = "0.0.43"
 
     val scalaCheck    = "1.14.0"
-    val scalaTest     = "3.0.5"
+    val scalaTest     = "3.0.6"
 
     val kindProjector = "0.9.9"
   }
