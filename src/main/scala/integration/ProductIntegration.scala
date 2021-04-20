1c007ce64fe45740a19ac7f1b4d7f14c191222ee
Scala Steward <me@scala-steward.org>
2021-04-20 20:25:48 +0200

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=series/3.x

--- src/main/scala/integration/ProductIntegration.scala
+++ src/main/scala/integration/ProductIntegration.scala
@@ -1,7 +1,7 @@
 package integration
 
 import cats.effect.syntax.concurrent._
-import cats.effect.{Concurrent, ContextShift, IO, Timer}
+import cats.effect.{Concurrent, IO}
 import cats.syntax.flatMap._
 import errors.PriceServiceError.{ProductErr, ProductPriceErr}
 import external._
@@ -12,6 +12,7 @@ import model.DomainModel._
 
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
+import cats.effect.{ Spawn, Temporal }
 
 sealed trait ProductIntegration[F[_]] {
   def product: ProductId => F[Option[Product]]
@@ -19,20 +20,18 @@ sealed trait ProductIntegration[F[_]] {
 }
 
 object ProductIntegration {
-  @inline def apply[F[_]: Concurrent: Timer: IO --> *[_]: Future --> *[_]](
+  @inline def apply[F[_]: Concurrent: Temporal: IO --> *[_]: Future --> *[_]](
     productDep: TeamTwoHttpApi,
     pricesDep: TeamOneHttpApi,
     t: FiniteDuration
-  )(
-    implicit CS: ContextShift[F]
   ): ProductIntegration[F] =
     new ProductIntegration[F] {
       def product: ProductId => F[Option[Product]] = { ps =>
-        CS.shift >> productDep.product(ps).adaptedTo[F].timeout(t).narrowFailureTo[ProductErr]
+        Spawn[F].cede >> productDep.product(ps).adaptedTo[F].timeout(t).narrowFailureTo[ProductErr]
       }
 
       def productPrice: Product => UserPreferences => F[Price] = { p => pref =>
-        CS.shift >> pricesDep.productPrice(p)(pref).adaptedTo[F].timeout(t).narrowFailureTo[ProductPriceErr]
+        Spawn[F].cede >> pricesDep.productPrice(p)(pref).adaptedTo[F].timeout(t).narrowFailureTo[ProductPriceErr]
       }
     }
 }
